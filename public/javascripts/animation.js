b60e53178c87ce5f38bf2bfcb169b3c2933554f0
unknown <coocies12@gmail.com>
2019-04-08 19:16:00 +0300

    add animation

---
+++ public/javascripts/animation.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    anime({
+        targets: '.menu-container',
+        delay: 1200,
+        opacity: 1
+    });
+
+    anime({
+        targets: '.play-content',
+        delay: 1700,
+        opacity: 1
+    });
+
+    anime({
+        targets: '.page-content--center--top',
+        delay: 2200,
+        opacity: 1
+    });
+
+    anime({
+        targets: '.page-content--center--bottom',
+        delay: 2700,
+        opacity: 1
+    });
+
+};
