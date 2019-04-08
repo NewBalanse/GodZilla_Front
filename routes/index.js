9bfe2fef422d6f5c2b0b47e301eec6ace254d0b1
unknown <coocies12@gmail.com>
2019-04-08 17:17:19 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+let anime = require('animejs');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Test ',
-      topText: 'god zilla'
-  });
+router.get('/', function (req, res, next) {
+
+    res.render('index', {
+        title: 'Test ',
+        topText: 'god zilla'
+    });
+
+
 });
 
 module.exports = router;
