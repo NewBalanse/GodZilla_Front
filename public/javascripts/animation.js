38aff05ccbcab7c05a221d1d4c63a931a4282f37
unknown <coocies12@gmail.com>
2019-04-10 02:30:20 +0300

    fix port

--- public/javascripts/animation.js
+++ public/javascripts/animation.js
@@ -1,22 +1,21 @@
 window.onload = function () {
+
     anime({
         targets: '.menu-container',
         delay: 1200,
         opacity: 1
     });
-
     anime({
         targets: '.play-content',
         delay: 1700,
         opacity: 1,
         translateY: -20
     });
-
     anime({
         targets: '.page-content--center--top',
         delay: 2200,
         opacity: 1,
-        translateX:20
+        translateX: 20
     });
 
     anime({
@@ -24,5 +23,6 @@ window.onload = function () {
         delay: 2700,
         opacity: 1
     });
+
 
 };
